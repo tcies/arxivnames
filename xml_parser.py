f2d4fc4f9118dc346dafa798fa8b76d8b438fbe9
Titus Cieslewski <dev@titus-c.ch>
2019-06-24 13:39:02 +0200

    WIP

--- xml_parser.py
+++ xml_parser.py
@@ -1,6 +1,24 @@
+# import ipdb; ipdb.set_trace()
+import json
 import xmltodict
+from IPython.core import ultratb
+import sys
+
+
+sys.excepthook = ultratb.FormattedTB(
+color_scheme='Linux', call_pdb=1)
+
 
 with open('results.xml') as fd:
     doc = xmltodict.parse(fd.read())
 
-print doc['feed']
+things = []  
+for entry in doc['feed']['entry']:
+   if len(entry['author']) > 1:
+     authors = [i['name'] for i in entry['author']]
+   else:
+     authors = entry['author']['name']
+   things.append(authors)
+
+with open('authors.json', 'w') as fd:
+   fd.write(json.dumps(things))
