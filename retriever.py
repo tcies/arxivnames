3dce7caab9c1bc59d3fd2a79ccf56f75fef80618
Titus Cieslewski <dev@titus-c.ch>
2019-06-24 14:29:43 +0200

    went to 10k

--- retriever.py
+++ retriever.py
@@ -1,4 +1,4 @@
 import urllib
-url = 'http://export.arxiv.org/api/query?search_query=all&start=0&max_results=100'
+url = 'http://export.arxiv.org/api/query?search_query=all&start=0&max_results=10000'
 data = urllib.urlopen(url).read()
 print data
